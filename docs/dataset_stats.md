49c44c5f0ad55a403a6208b1d915b90a39b9aff9
Punyashri <punyashrigowda16@gmail.com>
2026-06-14 13:53:28 +0530

    completed dataset statistics and preprocessing updates

---
+++ docs/dataset_stats.md
@@ -0,0 +1,41 @@
+# Dataset Statistics
+
+Dataset Name: ImageOASIS
+
+Dataset Link:
+https://www.kaggle.com/datasets/ninadaithal/imagesoasis
+
+Number of Classes: 4
+
+Classes:
+
+- NonDemented: 67222
+- VeryMildDemented: 13725
+- MildDemented: 5002
+- ModerateDemented: 488
+
+Total Images: 86437
+
+Dataset Characteristics:
+
+- MRI Brain Images
+- Highly imbalanced dataset
+- Suitable for Alzheimer's classification
+- Suitable for early cognitive decline detection
+
+Folder Structure:
+
+data/raw/images/
+
+Labels:
+
+0 - NonDemented
+1 - VeryMildDemented
+2 - MildDemented
+3 - ModerateDemented
+
+
+MildDemented 5002
+ModerateDemented 488
+NonDemented 67222
+VeryMildDemented 13725
--- preprocessing/image_preprocessing.py
+++ preprocessing/image_preprocessing.py
@@ -1,35 +1,39 @@
 import cv2
 import os
 
-input_dir = "data/raw/images"
-output_dir = "data/processed"
+INPUT_DIR = "data/raw/images"
+OUTPUT_DIR = "data/processed"
 
 IMG_SIZE = 128
 
-for cls in os.listdir(input_dir):
+for class_name in os.listdir(INPUT_DIR):
 
-    class_path = os.path.join(input_dir, cls)
+    class_path = os.path.join(INPUT_DIR, class_name)
 
     if not os.path.isdir(class_path):
         continue
 
-    output_class = os.path.join(output_dir, cls)
+    output_class = os.path.join(OUTPUT_DIR, class_name)
     os.makedirs(output_class, exist_ok=True)
 
-    for img_name in os.listdir(class_path):
+    for image_name in os.listdir(class_path):
 
-        img_path = os.path.join(class_path, img_name)
+        image_path = os.path.join(class_path, image_name)
 
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(image_path)
 
         if img is None:
+            print(f"Skipped: {image_name}")
             continue
 
+        # Convert to grayscale
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Resize
         img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
 
-        cv2.imwrite(
-            os.path.join(output_class, img_name),
-            img
-        )
+        save_path = os.path.join(output_class, image_name)
 
-print("Preprocessing completed")
+        cv2.imwrite(save_path, img)
+
+print("Preprocessing completed!")
