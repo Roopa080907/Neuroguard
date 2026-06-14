02b07b51eb127e689c3aacde333802966007a91a
Roopa080907 <roopapurushotham0809@gmail.com>
2026-06-14 13:41:40 +0530

    updated

--- model/predict.py
+++ model/predict.py
@@ -1,24 +1,36 @@
 import torch
 from torchvision import transforms
 from PIL import Image
-from model import AlzheimerCNN
+from model import AlzheimerSNN
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = AlzheimerCNN().to(device)
-model.load_state_dict(torch.load("alzheimer_model.pth", map_location=device))
+# --------------------
+# LOAD SNN MODEL
+# --------------------
+model = AlzheimerSNN().to(device)
+model.load_state_dict(torch.load("alzheimer_snn_model.pth", map_location=device))
 model.eval()
 
+# --------------------
+# CLASSES
+# --------------------
 classes = ['Mild Dementia', 'Moderate Dementia', 'Non Demented']
 
+# --------------------
+# TRANSFORM
+# --------------------
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
     transforms.Grayscale(num_output_channels=1),
     transforms.ToTensor()
 ])
 
+# --------------------
+# PREDICT FUNCTION
+# --------------------
 def predict_image(image_path):
-    image = Image.open(image_path)
+    image = Image.open(image_path).convert("RGB")
     image = transform(image).unsqueeze(0).to(device)
 
     with torch.no_grad():
